8c47360e07d0f472e0bd45a0dd504ca70c9ed99d
saisashreek <saisashreekd@gmail.com>
2023-08-21 13:45:46 +0530

    Added New Packeges

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ["class", '[data-mode="dark"]'],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
