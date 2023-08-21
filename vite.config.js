cd3e4d8acd613cc450854d989f5df78cddab1d00
saisashreek <saisashreekd@gmail.com>
2023-08-21 12:22:59 +0530

    Vite PWA added to project.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import { registerSW } from "virtual:pwa-register";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        cleanupOutdatedCaches: false,
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        sourcemap: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+      updateSW,
+    }),
+  ],
+});
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
